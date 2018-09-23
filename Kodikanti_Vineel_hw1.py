b3ea86f4cc634c26f2aa1d37ac049d2f3f5b2a38
Vineel Kodikanti <vineel.kodikanti@utexas.edu>
2018-09-23 00:40:20 -0500

    punct removal and stopword removal added

--- Kodikanti_Vineel_hw1.py
+++ Kodikanti_Vineel_hw1.py
@@ -5,9 +5,14 @@
 # Option chosen: A
 ###
 import nltk
+import string
 
-witwfile = open('windinthewillows.txt',"r")
-witw = witwfile.read()
+
+#Open and save file as string 
+with  open('windinthewillows.txt',"r",encoding = 'utf_8', errors = 'ignore') as witwfile:
+    witw = witwfile.read()
+
+#Tokenize Sentences
 #The tokenizer adds some identification information at the first index 
 #In this example it was '\xef\xbb\...'
 #There is also a sequence repeated within the sentences after tokenization
@@ -19,7 +24,18 @@ witw_sent = nltk.sent_tokenize(witw)
 #Punctuation is treated as words
 witw_words = nltk.word_tokenize(witw)
 
-print(witw_words)
+#Stopwords
+# the contents of the stop variable is a list which contains preprositions 
+# these prepositions may not aid in understand of sentence because many of them are related to possesion, linkage, etc. 
 
+stop =  nltk.corpus.stopwords.words("english")
 
+#remove stopwords from witw_words
 
+witw_goodwords = [i for i in witw_words if i  not in stop]
+
+#remove punctuation
+
+witw_goodwords_nopunct = [i for i in witw_words if i not in string.punctuation]
+
+print(witw_goodwords_nopunct)
